01c0d8c0f2912860e604c5f8203722c8f92eea8d
balebom <balebom@gmail.com>
2022-01-04 12:38:13 +0700

    update

--- MathFunctions/mysqrt.h
+++ MathFunctions/mysqrt.h
@@ -1,6 +1,16 @@
 #ifndef __MATH_FUNCTIONS_MYSQRT_H__
 #define __MATH_FUNCTIONS_MYSQRT_H__
 
+#if defined(_WIN32)
+#  if defined(EXPORTING_MYMATH)
+#    define DECLSPEC __declspec(dllexport)
+#  else
+#    define DECLSPEC __declspec(dllimport)
+#  endif
+#else // non windows
+#  define DECLSPEC
+#endif
+
 double CalSqrt(double x);
 double CalLogExp(double x);
 
